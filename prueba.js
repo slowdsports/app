c8e75e5e418f1b8e8150f11e98867fded56bc22c
slowdsports <72911530+slowdsports@users.noreply.github.com>
2020-11-23 23:15:12 -0600

    .

---
+++ prueba.js
@@ -0,0 +1,8 @@
+let mxtigres = `<figure class="figure">
+<img class="figure img-fluid competition tigres" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Tigres
+</figcaption>
+</figure>`;
+
+for(const ele of document.getElementsByClassName("tigres")){ele.innerHTML=(mxtigres)};
