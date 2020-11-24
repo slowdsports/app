dda79e67925e0b6f8a47b66c7d05ba8e146f5a39
slowdsports <72911530+slowdsports@users.noreply.github.com>
2020-11-24 11:01:23 -0600

    .

---
+++ js/canales.js
@@ -0,0 +1,7 @@
+let TUDN = `
+<a href="go:tudne">
+<li class="list-group-item">
+<i class="mx flag"></i> TUDN | HD</li>
+</a>
+`
+for(const ele of document.getElementsByClassName("ch-tudne")){ele.innerHTML=(TUDN)};
---
+++ js/platzi.js
@@ -0,0 +1,14 @@
+// Nativo
+// let edad = prompt("¿Cuál es tu año de nacimiento?");
+// let anoActual = 2080;
+// let anoNacimiento = edad;
+// let resultado = anoActual - edad;
+// document.write("Tienes: " +resultado+ " años de edad");
+
+// Función
+let btn = document.getElementById("boton");
+btn.addEventListener("click", btnFuncion);
+
+function btnFuncion(){
+    alert("Ya sos todo un pro");
+}
