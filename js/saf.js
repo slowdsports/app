850f1e536f868b8637f47fbb81a0837a4b723846
slowdsports <72911530+slowdsports@users.noreply.github.com>
2020-11-24 23:06:37 -0600

    .

---
+++ js/saf.js
@@ -0,0 +1,217 @@
+let tucuman = `
+<figure class="figure">
+<img class="figure img-fluid competition tucuman" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Tucumán
+</figcaption>
+</figure>
+`;
+let union = `
+<figure class="figure">
+<img class="figure img-fluid competition union" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Unión
+</figcaption>
+</figure>
+`;
+let arsenal = `
+<figure class="figure">
+<img class="figure img-fluid competition arsenal" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Arsenal
+</figcaption>
+</figure>
+`;
+let racing = `
+<figure class="figure">
+<img class="figure img-fluid competition racing" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Racing
+</figcaption>
+</figure>
+`;
+let colon = `
+<figure class="figure">
+<img class="figure img-fluid competition colon" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Colón
+</figcaption>
+</figure>
+`;
+let independiente = `
+<figure class="figure">
+<img class="figure img-fluid competition independiente" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Independiente
+</figcaption>
+</figure>
+`;
+let cordoba = `
+<figure class="figure">
+<img class="figure img-fluid competition cordoba" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Córdoba
+</figcaption>
+</figure>
+`;
+let justicia = `
+<figure class="figure">
+<img class="figure img-fluid competition justicia" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Justicia
+</figcaption>
+</figure>
+`;
+let banfield = `
+<figure class="figure">
+<img class="figure img-fluid competition banfield" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Banfield
+</figcaption>
+</figure>
+`;
+let river = `
+<figure class="figure">
+<img class="figure img-fluid competition river" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+River
+</figcaption>
+</figure>
+`;
+let rosario = `
+<figure class="figure">
+<img class="figure img-fluid competition rosario" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Rosario
+</figcaption>
+</figure>
+`;
+let godoy = `
+<figure class="figure">
+<img class="figure img-fluid competition godoy" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Godoy
+</figcaption>
+</figure>
+`;
+let talleres = `
+<figure class="figure">
+<img class="figure img-fluid competition talleres" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Tallleres
+</figcaption>
+</figure>
+`;
+let boca = `
+<figure class="figure">
+<img class="figure img-fluid competition boca" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Boca
+</figcaption>
+</figure>
+`;
+let lanus = `
+<figure class="figure">
+<img class="figure img-fluid competition lanus" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Lanús
+</figcaption>
+</figure>
+`;
+let newells = `
+<figure class="figure">
+<img class="figure img-fluid competition newells" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+NOB
+</figcaption>
+</figure>
+`;
+let sanlorenzo = `
+<figure class="figure">
+<img class="figure img-fluid competition sanlorenzo" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+San Lorenzo
+</figcaption>
+</figure>
+`;
+let aldosivi = `
+<figure class="figure">
+<img class="figure img-fluid competition aldosivi" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Aldosivi
+</figcaption>
+</figure>
+`;
+let argentinos = `
+<figure class="figure">
+<img class="figure img-fluid competition argentinos" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Argentinos
+</figcaption>
+</figure>
+`;
+let estudiantes = `
+<figure class="figure">
+<img class="figure img-fluid competition estudiantes" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Estudiantes
+</figcaption>
+</figure>
+`;
+let velez = `
+<figure class="figure">
+<img class="figure img-fluid competition velez" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Vélez
+</figcaption>
+</figure>
+`;
+let huracan = `
+<figure class="figure">
+<img class="figure img-fluid competition huracan" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Huracán
+</figcaption>
+</figure>
+`;
+let gimnasia = `
+<figure class="figure">
+<img class="figure img-fluid competition gimnasia" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Gimnasia
+</figcaption>
+</figure>
+`;
+let patronato = `
+<figure class="figure">
+<img class="figure img-fluid competition patronato" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Patronato
+</figcaption>
+</figure>
+`;
+
+for (const ele of document.getElementsByClassName("saf-tucuman")){ele.innerHTML=(tucuman)}
+for (const ele of document.getElementsByClassName("saf-union")){ele.innerHTML=(union)}
+for (const ele of document.getElementsByClassName("saf-arsenal")){ele.innerHTML=(arsenal)}
+for (const ele of document.getElementsByClassName("saf-racing")){ele.innerHTML=(racing)}
+for (const ele of document.getElementsByClassName("saf-colon")){ele.innerHTML=(colon)}
+for (const ele of document.getElementsByClassName("saf-independiente")){ele.innerHTML=(independiente)}
+for (const ele of document.getElementsByClassName("saf-cordoba")){ele.innerHTML=(cordoba)}
+for (const ele of document.getElementsByClassName("saf-justicia")){ele.innerHTML=(justicia)}
+for (const ele of document.getElementsByClassName("saf-banfield")){ele.innerHTML=(banfield)}
+for (const ele of document.getElementsByClassName("saf-river")){ele.innerHTML=(river)}
+for (const ele of document.getElementsByClassName("saf-rosario")){ele.innerHTML=(rosario)}
+for (const ele of document.getElementsByClassName("saf-godoy")){ele.innerHTML=(godoy)}
+for (const ele of document.getElementsByClassName("saf-talleres")){ele.innerHTML=(talleres)}
+for (const ele of document.getElementsByClassName("saf-boca")){ele.innerHTML=(boca)}
+for (const ele of document.getElementsByClassName("saf-lanus")){ele.innerHTML=(lanus)}
+for (const ele of document.getElementsByClassName("saf-newells")){ele.innerHTML=(newells)}
+for (const ele of document.getElementsByClassName("saf-sanlorenzo")){ele.innerHTML=(sanlorenzo)}
+for (const ele of document.getElementsByClassName("saf-aldosivi")){ele.innerHTML=(aldosivi)}
+for (const ele of document.getElementsByClassName("saf-argentinos")){ele.innerHTML=(argentinos)}
+for (const ele of document.getElementsByClassName("saf-estudiantes")){ele.innerHTML=(estudiantes)}
+for (const ele of document.getElementsByClassName("saf-velez")){ele.innerHTML=(velez)}
+for (const ele of document.getElementsByClassName("saf-huracan")){ele.innerHTML=(huracan)}
+for (const ele of document.getElementsByClassName("saf-gimnasia")){ele.innerHTML=(gimnasia)}
+for (const ele of document.getElementsByClassName("saf-patronato")){ele.innerHTML=(patronato)}
