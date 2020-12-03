c6bf481f84333659818ee42eb38db8e17bed4126
Eduardo <eperdomo833@gmail.com>
2020-12-03 09:21:51 -0600

    Create sudamerica.js

---
+++ nuevo - copia/sections/sudamerica.js
@@ -0,0 +1,10 @@
+let medellin = `
+<figure class="figure">
+<img class="figure img-fluid competition lib-medellin" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Medellín
+</figcaption>
+</figure>
+`;
+
+for (const ele of document.getElementsByClassName("suda-medellin")){ele.innerHTML=(medellin)}
