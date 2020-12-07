051cf3b7fba934d1b7d4c7c134965f524718f179
slowdsports <72911530+slowdsports@users.noreply.github.com>
2020-12-07 10:47:43 -0600

    .

--- sections/premier.js
+++ sections/premier.js
@@ -106,7 +106,7 @@ let leeds = `
 <figure class="figure">
 <img class="figure img-fluid competition leeds" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
 <figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
-Jets
+Leeds
 </figcaption>
 </figure>
 `;
