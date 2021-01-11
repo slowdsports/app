512ccba6998beaab6eda4f913bae2729bfc62ff1
Eduardo <eperdomo833@gmail.com>
2021-01-11 16:11:43 -0600

    Update laliga.js

--- sections/laliga.js
+++ sections/laliga.js
@@ -74,7 +74,7 @@ let elche = `
 <figure class="figure">
 <img class="figure img-fluid competition elche" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
 <figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
-Eclhe
+Elche
 </figcaption>
 </figure>
 `;
