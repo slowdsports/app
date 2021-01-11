f23ca9a22494b03d2f8d2a34efd6ede0125e009f
Eduardo <eperdomo833@gmail.com>
2021-01-11 10:58:24 -0600

    Update bundesliga.js

--- sections/bundesliga.js
+++ sections/bundesliga.js
@@ -146,7 +146,7 @@ let holstein = `
 <figure class="figure">
 <img class="figure img-fluid competition holstein" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
 <figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
-holstein
+Holstein
 </figcaption>
 </figure>
 `;
