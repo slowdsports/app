d9a8d0b42ea265450da6a21e9faf1323d43d38da
Eduardo <eperdomo833@gmail.com>
2021-01-13 10:22:15 -0600

    Update seriea.js

--- sections/seriea.js
+++ sections/seriea.js
@@ -158,6 +158,14 @@ Crotone
 </figcaption>
 </figure>
 `;
+let empoli = `
+<figure class="figure">
+<img class="figure img-fluid competition empoli" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Empoli
+</figcaption>
+</figure>
+`;
 
 for (const ele of document.getElementsByClassName("serie-milan")){ele.innerHTML=(milan)}
 for (const ele of document.getElementsByClassName("serie-sassuolo")){ele.innerHTML=(sassuolo)}
