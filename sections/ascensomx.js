17b7e8e05d4dd7e16e5df6f219a6608ec0104fd1
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-01-19 15:39:33 -0600

    Ascenso

---
+++ sections/ascensomx.js
@@ -0,0 +1,198 @@
+let tapatio = `
+<figure class="figure">
+<img class="figure img-fluid competition tapatio" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Tapatío
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("mx-tapatio")){ele.innerHTML=(tapatio)};
+
+let correcaminos = `
+<figure class="figure">
+<img class="figure img-fluid competition correcaminos" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Correcaminos
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("mx-correcaminos")){ele.innerHTML=(correcaminos)};
+
+let atlante = `
+<figure class="figure">
+<img class="figure img-fluid competition atlante" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Atlante
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("mx-atlante")){ele.innerHTML=(atlante)};
+
+let mineros = `
+<figure class="figure">
+<img class="figure img-fluid competition mineros" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Mineros
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("mx-mineros")){ele.innerHTML=(mineros)};
+
+let cimarrones = `
+<figure class="figure">
+<img class="figure img-fluid competition cimarrones" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Cimarrones
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("mx-cimarrones")){ele.innerHTML=(cimarrones)};
+
+let celaya = `
+<figure class="figure">
+<img class="figure img-fluid competition celaya" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Celaya
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("mx-celaya")){ele.innerHTML=(celaya)};
+
+let dorados = `
+<figure class="figure">
+<img class="figure img-fluid competition dorados" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Dorados
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("mx-dorados")){ele.innerHTML=(dorados)};
+
+let leonesnegros = `
+<figure class="figure">
+<img class="figure img-fluid competition leonesnegros" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Leones
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("mx-leonesnegros")){ele.innerHTML=(leonesnegros)};
+
+let tepatitlan = `
+<figure class="figure">
+<img class="figure img-fluid competition tepatitlan" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Tepatitlán
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("mx-tepatitlan")){ele.innerHTML=(tepatitlan)};
+
+let tlaxcala = `
+<figure class="figure">
+<img class="figure img-fluid competition tlaxcala" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Tlaxcala
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("mx-tlaxcala")){ele.innerHTML=(tlaxcala)};
+
+let alebrijes = `
+<figure class="figure">
+<img class="figure img-fluid competition alebrijes" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Alebrijes
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("mx-alebrijes")){ele.innerHTML=(alebrijes)};
+
+let atleticomorelia = `
+<figure class="figure">
+<img class="figure img-fluid competition atleticomorelia" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+At. Morelia
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("mx-atleticomorelia")){ele.innerHTML=(atleticomorelia)};
+
+let tampico = `
+<figure class="figure">
+<img class="figure img-fluid competition tampico" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Tampico
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("mx-tampico")){ele.innerHTML=(tampico)};
+
+let madero = `
+<figure class="figure">
+<img class="figure img-fluid competition tampico" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Tampico
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("mx-madero")){ele.innerHTML=(madero)};
+
+let venados = `
+<figure class="figure">
+<img class="figure img-fluid competition venados" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Venados
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("mx-venados")){ele.innerHTML=(venados)};
+
+let pumastabasco = `
+<figure class="figure">
+<img class="figure img-fluid competition pumastabasco" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Tabasco
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("mx-pumastabasco")){ele.innerHTML=(pumastabasco)};
+
+let tabasco = `
+<figure class="figure">
+<img class="figure img-fluid competition pumastabasco" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Tabasco
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("mx-tabasco")){ele.innerHTML=(tabasco)};
+
+let cancun = `
+<figure class="figure">
+<img class="figure img-fluid competition cancun" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Cancún
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("mx-cancun")){ele.innerHTML=(cancun)};
+
