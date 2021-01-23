655aef6e97453cabc3d8ab84c9b11f1f56708a5e
Eduardo <eperdomo833@gmail.com>
2021-01-23 08:41:53 -0600

    Update sudamerica.js

--- sections/sudamerica.js
+++ sections/sudamerica.js
@@ -1,10 +1,19 @@
-let medellin = `
+let lanus = `
 <figure class="figure">
-<img class="figure img-fluid competition lib-medellin" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<img class="figure img-fluid competition suda-lanus" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
 <figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
-Medellín
+Lanus
+</figcaption>
+</figure>
+`;
+let defensa = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-defensa" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Defensa
 </figcaption>
 </figure>
 `;
 
-for (const ele of document.getElementsByClassName("suda-medellin")){ele.innerHTML=(medellin)}
+for (const ele of document.getElementsByClassName("suda-lanus")){ele.innerHTML=(lanus)}
+for (const ele of document.getElementsByClassName("suda-defensa")){ele.innerHTML=(defensayjusticia)}
