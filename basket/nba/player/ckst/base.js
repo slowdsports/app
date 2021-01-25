8155543a9be053da6ab2af8450b5d7ab0095ebf1
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-01-25 17:18:16 -0600

    NBA

---
+++ basket/nba/player/ckst/base.js
@@ -0,0 +1,4 @@
+var base = "//crackstreams.is/nbastreams/game-";
+var end = "/video3.php";
+var fuente = base + num + end;
+document.write(" <iframe  class='ckst' frameborder='0' height=100% width=100% src='" + fuente + "' allowfullscreen='yes' scrolling='no' allowtransparency></iframe>");
