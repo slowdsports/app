e2f89b91cca5061b2d616f3ea8132decb5f48226
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-01-27 19:29:31 -0600

    FPD

---
+++ sections/fpd.js
@@ -0,0 +1,131 @@
+let saprissa = `
+<figure class="figure">
+<img class="figure img-fluid competition saprissa" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Saprissa
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("fpd-saprisa")){ele.innerHTML=(saprisa)};
+
+let herdiano = `
+<figure class="figure">
+<img class="figure img-fluid competition herdiano" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Herdiano
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("fpd-herdiano")){ele.innerHTML=(herediano)};
+
+let cartagines = `
+<figure class="figure">
+<img class="figure img-fluid competition cartagines" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Cartaginés
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("fpd-cartagines")){ele.innerHTML=(cartagines)};
+
+let guadalupe = `
+<figure class="figure">
+<img class="figure img-fluid competition guadalupe" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Guadalupe
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("fpd-guadalupe")){ele.innerHTML=(guadalupe)};
+
+let jicaral = `
+<figure class="figure">
+<img class="figure img-fluid competition jicaral" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Jicaral
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("fpd-sancarlos")){ele.innerHTML=(jicaral)};
+
+let sancarlos = `
+<figure class="figure">
+<img class="figure img-fluid competition sancarlos" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+San Carlos
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("fpd-sancarlos")){ele.innerHTML=(sancarlos)};
+
+let grecia = `
+<figure class="figure">
+<img class="figure img-fluid competition grecia" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Grecia
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("fpd-grecia")){ele.innerHTML=(grecia)};
+
+let perezzeledon = `
+<figure class="figure">
+<img class="figure img-fluid competition perezzeledon" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Zeledón
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("fpd-perezzeledon")){ele.innerHTML=(perezzeledon)};
+
+let santos = `
+<figure class="figure">
+<img class="figure img-fluid competition santos" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Santos
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("fpd-santos")){ele.innerHTML=(santos)};
+
+let limon = `
+<figure class="figure">
+<img class="figure img-fluid competition limon" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Limón
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("fpd-limon")){ele.innerHTML=(limon)};
+
+let lau = `
+<figure class="figure">
+<img class="figure img-fluid competition lau" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+La U
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("fpd-lau")){ele.innerHTML=(lau)};
+
+let alajuelense = `
+<figure class="figure">
+<img class="figure img-fluid competition alajuelense" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Alajuelense
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("fpd-alajuelense")){ele.innerHTML=(alajuelense)};
