e9b8dcd93299652e7f21e3cfc8af0ff3a0c50b6b
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-01-28 17:58:44 -0600

    NBA + Primera UY

---
+++ sections/ligauy.js
@@ -0,0 +1,176 @@
+let nacional = `
+<figure class="figure">
+<img class="figure img-fluid competition nacional" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Nacional
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("uru-nacional")){ele.innerHTML=(nacional)};
+
+let torque = `
+<figure class="figure">
+<img class="figure img-fluid competition torque" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Torque
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("uru-torque")){ele.innerHTML=(torque)};
+
+let rentistas = `
+<figure class="figure">
+<img class="figure img-fluid competition rentistas" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Rentistas
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("uru-rentistas")){ele.innerHTML=(rentistas)};
+
+let cerrolargo = `
+<figure class="figure">
+<img class="figure img-fluid competition cerrolargo" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Cerro
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("uru-cerrolargo")){ele.innerHTML=(cerrolargo)};
+
+let wanderers = `
+<figure class="figure">
+<img class="figure img-fluid competition wanderers" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Wanderers
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("uru-wanderers")){ele.innerHTML=(wanderers)};
+
+let penarol = `
+<figure class="figure">
+<img class="figure img-fluid competition penarol" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Peñarol
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("uru-penarol")){ele.innerHTML=(penarol)};
+
+let liverpool = `
+<figure class="figure">
+<img class="figure img-fluid competition liverpool" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Liverpool
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("uru-liverpool")){ele.innerHTML=(liverpool)};
+
+let defensor = `
+<figure class="figure">
+<img class="figure img-fluid competition defensor" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Defensor
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("uru-defensor")){ele.innerHTML=(defensor)};
+
+let riveruy = `
+<figure class="figure">
+<img class="figure img-fluid competition riveruy" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+River
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("uru-riveruy")){ele.innerHTML=(riveruy)};
+
+let fenix = `
+<figure class="figure">
+<img class="figure img-fluid competition fenix" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Fénix
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("uru-fenix")){ele.innerHTML=(fenix)};
+
+let maldonado = `
+<figure class="figure">
+<img class="figure img-fluid competition maldonado" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Maldonado
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("uru-maldonado")){ele.innerHTML=(maldonado)};
+
+let progreso = `
+<figure class="figure">
+<img class="figure img-fluid competition progreso" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Progreso
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("uru-progreso")){ele.innerHTML=(progreso)};
+
+let boston = `
+<figure class="figure">
+<img class="figure img-fluid competition boston" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Boston
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("uru-boston")){ele.innerHTML=(boston)};
+
+let danubio = `
+<figure class="figure">
+<img class="figure img-fluid competition danubio" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Danubio
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("uru-danubio")){ele.innerHTML=(danubio)};
+
+let cerro = `
+<figure class="figure">
+<img class="figure img-fluid competition cerro" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Cerro
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("uru-cerro")){ele.innerHTML=(cerro)};
+
+let colonia = `
+<figure class="figure">
+<img class="figure img-fluid competition colonia" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Colonia
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("uru-colonia")){ele.innerHTML=(colonia)};
+
