fdd62a5d61f4b2cd3abf45baf4a47bca8fcaa984
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-01-28 19:28:33 -0600

    Update ligauy.js

--- sections/ligauy.js
+++ sections/ligauy.js
@@ -1,6 +1,6 @@
 let nacional = `
 <figure class="figure">
-<img class="figure img-fluid competition nacional" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<img class="figure img-fluid competition nacionaluy" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
 <figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
 Nacional
 </figcaption>
