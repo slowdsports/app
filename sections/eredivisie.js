1aecb6b382a2a3513f292ef36696fad5b8948bbd
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-01-28 21:23:56 -0600

    Create eredivisie.js

---
+++ sections/eredivisie.js
@@ -0,0 +1,198 @@
+let ajax = `
+<figure class="figure">
+<img class="figure img-fluid competition ajax" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Ajax
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("eredi-ajax")){ele.innerHTML=(ajax)};
+
+let psv = `
+<figure class="figure">
+<img class="figure img-fluid competition psv" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+PSV
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("eredi-psv")){ele.innerHTML=(psv)};
+
+let vitesse = `
+<figure class="figure">
+<img class="figure img-fluid competition vitesse" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Vitesse
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("eredi-vitesse")){ele.innerHTML=(vitesse)};
+
+let az = `
+<figure class="figure">
+<img class="figure img-fluid competition az" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+AZ
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("eredi-az")){ele.innerHTML=(az)};
+
+let feyenoord = `
+<figure class="figure">
+<img class="figure img-fluid competition feyenoord" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Feyenoord
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("eredi-feyenoord")){ele.innerHTML=(feyenoord)};
+
+let groningen = `
+<figure class="figure">
+<img class="figure img-fluid competition groningen" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Groningen
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("eredi-groningen")){ele.innerHTML=(groningen)};
+
+let twente = `
+<figure class="figure">
+<img class="figure img-fluid competition twente" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Twente
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("eredi-twente")){ele.innerHTML=(twente)};
+
+let utrecht = `
+<figure class="figure">
+<img class="figure img-fluid competition utrecht" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Utrecht
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("eredi-utrecht")){ele.innerHTML=(utrecht)};
+
+let heerenveen = `
+<figure class="figure">
+<img class="figure img-fluid competition heerenveen" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Heerenveen
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("eredi-heerenveen")){ele.innerHTML=(heerenveen)};
+
+let sparta = `
+<figure class="figure">
+<img class="figure img-fluid competition sparta" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Sparta
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("eredi-sparta")){ele.innerHTML=(sparta)};
+
+let heracles = `
+<figure class="figure">
+<img class="figure img-fluid competition heracles" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Heracles
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("eredi-heracles")){ele.innerHTML=(heracles)};
+
+let fortuna = `
+<figure class="figure">
+<img class="figure img-fluid competition fortuna" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Fortuna
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("eredi-fortuna")){ele.innerHTML=(fortuna)};
+
+let venlo = `
+<figure class="figure">
+<img class="figure img-fluid competition venlo" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Venlo
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("eredi-venlo")){ele.innerHTML=(venlo)};
+
+let zwolle = `
+<figure class="figure">
+<img class="figure img-fluid competition zwolle" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Zwolle
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("eredi-zwolle")){ele.innerHTML=(zwolle)};
+
+let waalwijk = `
+<figure class="figure">
+<img class="figure img-fluid competition waalwijk" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Waalwijk
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("eredi-waalwijk")){ele.innerHTML=(waalwijk)};
+
+let ado = `
+<figure class="figure">
+<img class="figure img-fluid competition ado" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+ADO
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("eredi-ado")){ele.innerHTML=(ado)};
+
+let willemii = `
+<figure class="figure">
+<img class="figure img-fluid competition willemii" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Willem II
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("eredi-willemii")){ele.innerHTML=(willemii)};
+
+let emmen = `
+<figure class="figure">
+<img class="figure img-fluid competition emmen" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Emmen
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("eredi-emmen")){ele.innerHTML=(emmen)};
+
