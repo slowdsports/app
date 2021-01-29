d75171cd9e83bb8801d403dc5ffa7d5cd16fc290
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-01-29 10:15:54 -0600

    Primera Chile

---
+++ sections/ligacl.js
@@ -0,0 +1,198 @@
+let ucatolica = `
+<figure class="figure">
+<img class="figure img-fluid competition ucatolica" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+U Católica
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("chil-ucatolica")){ele.innerHTML=(ucatolica)};
+
+let ulacalera = `
+<figure class="figure">
+<img class="figure img-fluid competition ulacalera" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+U La Calera
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("chil-ulacalera")){ele.innerHTML=(ulacalera)};
+
+let espanola = `
+<figure class="figure">
+<img class="figure img-fluid competition espanola" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Española
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("chil-espanola")){ele.innerHTML=(espanola)};
+
+let palestino = `
+<figure class="figure">
+<img class="figure img-fluid competition palestino" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Palestino
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("chil-palestino")){ele.innerHTML=(palestino)};
+
+let antofagasta = `
+<figure class="figure">
+<img class="figure img-fluid competition antofagasta" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Antofagasta
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("chil-antofagasta")){ele.innerHTML=(antofagasta)};
+
+let huachipato = `
+<figure class="figure">
+<img class="figure img-fluid competition huachipato" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Huachipato
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("chil-huachipato")){ele.innerHTML=(huachipato)};
+
+let udechile = `
+<figure class="figure">
+<img class="figure img-fluid competition udechile" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+U de Chile
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("chil-udechile")){ele.innerHTML=(udechile)};
+
+let curico = `
+<figure class="figure">
+<img class="figure img-fluid competition curico" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Curicó
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("chil-curico")){ele.innerHTML=(curico)};
+
+let ohiggins = `
+<figure class="figure">
+<img class="figure img-fluid competition ohiggins" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+O'Higgins
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("chil-ohiggins")){ele.innerHTML=(ohiggins)};
+
+let everton = `
+<figure class="figure">
+<img class="figure img-fluid competition evertoncl" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Everton
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("chil-everton")){ele.innerHTML=(everton)};
+
+let wanderers = `
+<figure class="figure">
+<img class="figure img-fluid competition wandererscl" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Wanderers
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("chil-wanderers")){ele.innerHTML=(wanderers)};
+
+let serena = `
+<figure class="figure">
+<img class="figure img-fluid competition serena" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Serena
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("chil-serena")){ele.innerHTML=(serena)};
+
+let cobresal = `
+<figure class="figure">
+<img class="figure img-fluid competition cobresal" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Cobresal
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("chil-cobresal")){ele.innerHTML=(cobresal)};
+
+let udeconcepcion = `
+<figure class="figure">
+<img class="figure img-fluid competition udeconcepcion" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Conecepción
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("chil-udeconcepcion")){ele.innerHTML=(udeconcepcion)};
+
+let audax = `
+<figure class="figure">
+<img class="figure img-fluid competition audax" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Audax
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("chil-audax")){ele.innerHTML=(audax)};
+
+let colocolo = `
+<figure class="figure">
+<img class="figure img-fluid competition colocolo" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Colo-Colo
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("chil-colocolo")){ele.innerHTML=(colocolo)};
+
+let iquique = `
+<figure class="figure">
+<img class="figure img-fluid competition iquique" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Iquique
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("chil-iquique")){ele.innerHTML=(iquique)};
+
+let coquimbo = `
+<figure class="figure">
+<img class="figure img-fluid competition coquimbo" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Coquimbo
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("chil-coquimbo")){ele.innerHTML=(coquimbo)};
+
