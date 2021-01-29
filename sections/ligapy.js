ff2a117c8f895f79b1aa86616f733e3070170184
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-01-28 18:13:11 -0600

    Liga PY

---
+++ sections/ligapy.js
@@ -0,0 +1,132 @@
+let octubre = `
+<figure class="figure">
+<img class="figure img-fluid competition octubre" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+12 Octubre
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("para-octubre")){ele.innerHTML=(nacional)};
+
+let nacional = `
+<figure class="figure">
+<img class="figure img-fluid competition nacionalpy" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Nacional
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("para-nacional")){ele.innerHTML=(nacional)};
+
+let cerro = `
+<figure class="figure">
+<img class="figure img-fluid competition cerro" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Cerro
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("para-cerro")){ele.innerHTML=(cerro)};
+
+let libertad = `
+<figure class="figure">
+<img class="figure img-fluid competition libertad" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Libertad
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("para-libertad")){ele.innerHTML=(libertad)};
+
+let guarani = `
+<figure class="figure">
+<img class="figure img-fluid competition guarani" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Guaraní
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("para-guarani")){ele.innerHTML=(guarani)};
+
+let olimpia = `
+<figure class="figure">
+<img class="figure img-fluid competition olimpiapy" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Olimpia
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("para-olimpia")){ele.innerHTML=(olimpia)};
+
+let sol = `
+<figure class="figure">
+<img class="figure img-fluid competition sol" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Sol
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("para-sol")){ele.innerHTML=(sol)};
+
+let guairena = `
+<figure class="figure">
+<img class="figure img-fluid competition guairena" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Guaireña
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("para-guairena")){ele.innerHTML=(guairena)};
+
+let sanlorenzo = `
+<figure class="figure">
+<img class="figure img-fluid competition sanlorenzo" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+San Lorenzo
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("para-sanlorenzo")){ele.innerHTML=(sanlorenzo)};
+
+let sportivo = `
+<figure class="figure">
+<img class="figure img-fluid competition sportivo" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Luqueño
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("para-sportivo")){ele.innerHTML=(sportivo)};
+
+let river = `
+<figure class="figure">
+<img class="figure img-fluid competition riverpy" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+River
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("para-river")){ele.innerHTML=(river)};
+
+let general = `
+<figure class="figure">
+<img class="figure img-fluid competition general" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+General
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("para-general")){ele.innerHTML=(general)};
+
