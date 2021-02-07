d1e3c8c2749dd3d9ea7cca2d91f3ef704c5ffe6a
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-02-07 08:47:35 -0600

    Update suda-libertadores2.js

--- sections/suda-libertadores2.js
+++ sections/suda-libertadores2.js
@@ -1,11 +1,10 @@
-let libertad = `
+let palmeiras = `
 <figure class="figure">
-<img class="figure img-fluid competition lib-libertad" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<img class="figure img-fluid competition lib-palmeiras" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
 <figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
-Libertad
+Palmeiras
 </figcaption>
 </figure>
 `;
 
-
-for (const ele of document.getElementsByClassName("lib-libertad")){ele.innerHTML=(libertad)};
+for (const ele of document.getElementsByClassName("liberta-palmeiras")){ele.innerHTML=(palmeiras)};
