a33453b6b37cfb33a6ca3e368dab971472c546fb
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-02-16 16:14:45 -0600

    Betplay

---
+++ sections/betplay.js
@@ -0,0 +1,265 @@
+let deportivocali = `
+<figure class="figure">
+<img class="figure img-fluid competition deportivocali" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Dep. Cali
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("betp-deportivocali")){ele.innerHTML=(deportivocali)};
+
+let junior = `
+<figure class="figure">
+<img class="figure img-fluid competition junior" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Junior
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("betp-junior")){ele.innerHTML=(junior)};
+
+let santafe = `
+<figure class="figure">
+<img class="figure img-fluid competition santafe" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Santa Fé
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("betp-santafe")){ele.innerHTML=(santafe)};
+
+let tolima = `
+<figure class="figure">
+<img class="figure img-fluid competition tolima" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Tolima
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("betp-tolima")){ele.innerHTML=(tolima)};
+
+let equidad = `
+<figure class="figure">
+<img class="figure img-fluid competition equidad" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Equidad
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("betp-equidad")){ele.innerHTML=(equidad)};
+
+let nacional = `
+<figure class="figure">
+<img class="figure img-fluid competition nacional" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Nacional
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("betp-nacional")){ele.innerHTML=(nacional)};
+for(const ele of document.getElementsByClassName("betp-atleticonacional")){ele.innerHTML=(nacional)};
+
+let millonarios = `
+<figure class="figure">
+<img class="figure img-fluid competition millonarios" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Millonarios
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("betp-millonarios")){ele.innerHTML=(millonarios)};
+
+let independientemedellin = `
+<figure class="figure">
+<img class="figure img-fluid competition independientemedellin" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Indep. Medellín
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("betp-independientemedellin")){ele.innerHTML=(independientemedellin)};
+
+let independiente = `
+<figure class="figure">
+<img class="figure img-fluid competition independientemedellin" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Indep. Medellín
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("betp-independiente")){ele.innerHTML=(independiente)};
+
+let jaguares = `
+<figure class="figure">
+<img class="figure img-fluid competition jaguares" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Jaguares
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("betp-jaguares")){ele.innerHTML=(jaguares)};
+
+let pasto = `
+<figure class="figure">
+<img class="figure img-fluid competition pasto" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Pasto
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("betp-pasto")){ele.innerHTML=(pasto)};
+
+let caldas = `
+<figure class="figure">
+<img class="figure img-fluid competition caldas" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Caldas
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("betp-caldas")){ele.innerHTML=(caldas)};
+
+let americacali = `
+<figure class="figure">
+<img class="figure img-fluid competition americacali" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Cali
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("betp-americacali")){ele.innerHTML=(americacali)};
+
+let rionegro = `
+<figure class="figure">
+<img class="figure img-fluid competition rionegro" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Águilas
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("betp-rionegro")){ele.innerHTML=(rionegro)};
+
+let aguilas = `
+<figure class="figure">
+<img class="figure img-fluid competition aguilas" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Águilas
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("betp-aguilas")){ele.innerHTML=(aguilas)};
+
+let bucaramanga = `
+<figure class="figure">
+<img class="figure img-fluid competition bucaramanga" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Bucaramanga
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("betp-bucaramanga")){ele.innerHTML=(bucaramanga)};
+
+let tapatio = `
+<figure class="figure">
+<img class="figure img-fluid competition tapatio" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Tapatío
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("betp-tapatio")){ele.innerHTML=(tapatio)};
+
+let patriotas = `
+<figure class="figure">
+<img class="figure img-fluid competition patriotas" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Patriotas
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("betp-patriotas")){ele.innerHTML=(patriotas)};
+
+let envigado = `
+<figure class="figure">
+<img class="figure img-fluid competition envigado" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Envigado
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("betp-envigado")){ele.innerHTML=(envigado)};
+
+let alianza = `
+<figure class="figure">
+<img class="figure img-fluid competition alianza" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Petrolera
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("betp-alianza")){ele.innerHTML=(alianza)};
+
+let petrolera = `
+<figure class="figure">
+<img class="figure img-fluid competition petrolera" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Petrolera
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("betp-petrolera")){ele.innerHTML=(petrolera)};
+
+let pereira = `
+<figure class="figure">
+<img class="figure img-fluid competition pereira" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Pereira
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("betp-pereira")){ele.innerHTML=(pereira)};
+
+let boyaca = `
+<figure class="figure">
+<img class="figure img-fluid competition chico" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Chicó
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("betp-boyaca")){ele.innerHTML=(boyaca)};
+
+let chico = `
+<figure class="figure">
+<img class="figure img-fluid competition chico" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Chicó
+</figcaption>
+</figure>
+`;
+
+for(const ele of document.getElementsByClassName("betp-chico")){ele.innerHTML=(chico)};
+
