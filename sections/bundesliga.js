33300d5419985b6642bb3bc1454a4f9e6d755b95
Eduardo <eperdomo833@gmail.com>
2021-02-15 23:23:04 -0600

    Update bundesliga.js

--- sections/bundesliga.js
+++ sections/bundesliga.js
@@ -142,15 +142,6 @@ Schalke
 </figcaption>
 </figure>
 `;
-let holstein = `
-<figure class="figure">
-<img class="figure img-fluid competition holstein" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
-<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
-Holstein
-</figcaption>
-</figure>
-`;
-
 
 for (const ele of document.getElementsByClassName("bund-bayern")){ele.innerHTML=(bayern)};
 for (const ele of document.getElementsByClassName("bund-dortmund")){ele.innerHTML=(dortmund)};
