4f06bd6e53bbfa59aa3f0c69c43a6ac7003f873a
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-02-19 15:41:13 -0600

    ResúmenesN baaaa

---
+++ basket/nba/channelbase.js
@@ -0,0 +1,159 @@
+let nba1 = `
+<a href="http://slowd.tk/live/nba/1.php">
+<li class="list-group-item">
+<i class="us flag"></i> NBA (1) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-1")){ele.innerHTML=(nba1)};
+
+let nba2 = `
+<a href="http://slowd.tk/live/nba/2.php">
+<li class="list-group-item">
+<i class="us flag"></i> NBA (2) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-2")){ele.innerHTML=(nba2)};
+
+let nba3 = `
+<a href="http://slowd.tk/live/nba/3.php">
+<li class="list-group-item">
+<i class="us flag"></i> NBA (3) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-3")){ele.innerHTML=(nba3)};
+
+let nba4 = `
+<a href="http://slowd.tk/live/nba/4.php">
+<li class="list-group-item">
+<i class="us flag"></i> NBA (4) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-4")){ele.innerHTML=(nba4)};
+
+let nba5 = `
+<a href="http://slowd.tk/live/nba/5.php">
+<li class="list-group-item">
+<i class="us flag"></i> NBA (5) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-5")){ele.innerHTML=(nba5)};
+
+let nba6 = `
+<a href="http://slowd.tk/live/nba/6.php">
+<li class="list-group-item">
+<i class="us flag"></i> NBA (6) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-6")){ele.innerHTML=(nba6)};
+
+let nba7 = `
+<a href="http://slowd.tk/live/nba/7.php">
+<li class="list-group-item">
+<i class="us flag"></i> NBA (7) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-7")){ele.innerHTML=(nba7)};
+
+let nba8 = `
+<a href="http://slowd.tk/live/nba/8.php">
+<li class="list-group-item">
+<i class="us flag"></i> NBA (8) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-8")){ele.innerHTML=(nba8)};
+
+let nba9 = `
+<a href="http://slowd.tk/live/nba/9.php">
+<li class="list-group-item">
+<i class="us flag"></i> NBA (9) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-9")){ele.innerHTML=(nba9)};
+
+let nba10 = `
+<a href="http://slowd.tk/live/nba/10.php">
+<li class="list-group-item">
+<i class="us flag"></i> NBA (10) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-10")){ele.innerHTML=(nba10)};
+
+let nba11 = `
+<a href="http://slowd.tk/live/nba/11.php">
+<li class="list-group-item">
+<i class="us flag"></i> NBA (11) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-11")){ele.innerHTML=(nba11)};
+
+let nba12 = `
+<a href="http://slowd.tk/live/nba/12.php">
+<li class="list-group-item">
+<i class="us flag"></i> NBA (12) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-12")){ele.innerHTML=(nba12)};
+
+let nba13 = `
+<a href="http://slowd.tk/live/nba/13.php">
+<li class="list-group-item">
+<i class="us flag"></i> NBA (13) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-13")){ele.innerHTML=(nba13)};
+
+let nba14 = `
+<a href="http://slowd.tk/live/nba/14.php">
+<li class="list-group-item">
+<i class="us flag"></i> NBA (14) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-14")){ele.innerHTML=(nba14)};
+
+let nba15 = `
+<a href="http://slowd.tk/live/nba/15.php">
+<li class="list-group-item">
+<i class="us flag"></i> NBA (15) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-15")){ele.innerHTML=(nba15)};
+
+let nba16 = `
+<a href="http://slowd.tk/live/nba/16.php">
+<li class="list-group-item">
+<i class="us flag"></i> NBA (16) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-16")){ele.innerHTML=(nba16)};
+
+let nba17 = `
+<a href="http://slowd.tk/live/nba/17.php">
+<li class="list-group-item">
+<i class="us flag"></i> NBA (17) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-17")){ele.innerHTML=(nba17)};
+
+let nba18 = `
+<a href="http://slowd.tk/live/nba/18.php">
+<li class="list-group-item">
+<i class="us flag"></i> NBA (18) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-18")){ele.innerHTML=(nba18)};
+
+let nba19 = `
+<a href="http://slowd.tk/live/nba/19.php">
+<li class="list-group-item">
+<i class="us flag"></i> NBA (19) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-19")){ele.innerHTML=(nba19)};
+
+let nba20 = `
+<a href="http://slowd.tk/live/nba/20.php">
+<li class="list-group-item">
+<i class="us flag"></i> NBA (20) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-20")){ele.innerHTML=(nba20)};
