2ced4ad92e67823f05937fc28f9f732745f4dff3
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-03-17 11:51:49 -0600

    asdasd

--- basket/nba/channelbase.js
+++ basket/nba/channelbase.js
@@ -7,7 +7,7 @@ let fnl = "/playlist.m3u8";
 //console.log(xurl);
 
 let nba1 = `
-<a href="go:mx1">
+<a href="go:nba1">
 <li class="list-group-item">
 <i class="us flag"></i> NBA (1) | HD</li>
 </a>
@@ -15,7 +15,7 @@ let nba1 = `
 for(const ele of document.getElementsByClassName("nba-1")){ele.innerHTML=(nba1)};
 
 let nba2 = `
-<a href="go:mx2">
+<a href="go:nba2">
 <li class="list-group-item">
 <i class="us flag"></i> NBA (2) | HD</li>
 </a>
@@ -23,7 +23,7 @@ let nba2 = `
 for(const ele of document.getElementsByClassName("nba-2")){ele.innerHTML=(nba2)};
 
 let nba3 = `
-<a href="go:mx3">
+<a href="go:nba3">
 <li class="list-group-item">
 <i class="us flag"></i> NBA (3) | HD</li>
 </a>
@@ -31,7 +31,7 @@ let nba3 = `
 for(const ele of document.getElementsByClassName("nba-3")){ele.innerHTML=(nba3)};
 
 let nba4 = `
-<a href="go:mx4">
+<a href="go:nba4">
 <li class="list-group-item">
 <i class="us flag"></i> NBA (4) | HD</li>
 </a>
@@ -39,7 +39,7 @@ let nba4 = `
 for(const ele of document.getElementsByClassName("nba-4")){ele.innerHTML=(nba4)};
 
 let nba5 = `
-<a href="go:mx5">
+<a href="go:nba5">
 <li class="list-group-item">
 <i class="us flag"></i> NBA (5) | HD</li>
 </a>
@@ -47,7 +47,7 @@ let nba5 = `
 for(const ele of document.getElementsByClassName("nba-5")){ele.innerHTML=(nba5)};
 
 let nba6 = `
-<a href="go:mx6">
+<a href="go:nba6">
 <li class="list-group-item">
 <i class="us flag"></i> NBA (6) | HD</li>
 </a>
@@ -55,7 +55,7 @@ let nba6 = `
 for(const ele of document.getElementsByClassName("nba-6")){ele.innerHTML=(nba6)};
 
 let nba7 = `
-<a href="go:mx7">
+<a href="go:nba7">
 <li class="list-group-item">
 <i class="us flag"></i> NBA (7) | HD</li>
 </a>
@@ -63,7 +63,7 @@ let nba7 = `
 for(const ele of document.getElementsByClassName("nba-7")){ele.innerHTML=(nba7)};
 
 let nba8 = `
-<a href="go:mx8">
+<a href="go:nba8">
 <li class="list-group-item">
 <i class="us flag"></i> NBA (8) | HD</li>
 </a>
@@ -71,7 +71,7 @@ let nba8 = `
 for(const ele of document.getElementsByClassName("nba-8")){ele.innerHTML=(nba8)};
 
 let nba9 = `
-<a href="go:mx9">
+<a href="go:nba9">
 <li class="list-group-item">
 <i class="us flag"></i> NBA (9) | HD</li>
 </a>
@@ -79,7 +79,7 @@ let nba9 = `
 for(const ele of document.getElementsByClassName("nba-9")){ele.innerHTML=(nba9)};
 
 let nba10 = `
-<a href="go:mx10">
+<a href="go:nba10">
 <li class="list-group-item">
 <i class="us flag"></i> NBA (10) | HD</li>
 </a>
@@ -87,7 +87,7 @@ let nba10 = `
 for(const ele of document.getElementsByClassName("nba-10")){ele.innerHTML=(nba10)};
 
 let nba11 = `
-<a href="go:mx11">
+<a href="go:nba11">
 <li class="list-group-item">
 <i class="us flag"></i> NBA (11) | HD</li>
 </a>
@@ -95,7 +95,7 @@ let nba11 = `
 for(const ele of document.getElementsByClassName("nba-11")){ele.innerHTML=(nba11)};
 
 let nba12 = `
-<a href="go:mx12">
+<a href="go:nba12">
 <li class="list-group-item">
 <i class="us flag"></i> NBA (12) | HD</li>
 </a>
@@ -103,7 +103,7 @@ let nba12 = `
 for(const ele of document.getElementsByClassName("nba-12")){ele.innerHTML=(nba12)};
 
 let nba13 = `
-<a href="go:mx13">
+<a href="go:nba13">
 <li class="list-group-item">
 <i class="us flag"></i> NBA (13) | HD</li>
 </a>
@@ -111,7 +111,7 @@ let nba13 = `
 for(const ele of document.getElementsByClassName("nba-13")){ele.innerHTML=(nba13)};
 
 let nba14 = `
-<a href="go:mx14">
+<a href="go:nba14">
 <li class="list-group-item">
 <i class="us flag"></i> NBA (14) | HD</li>
 </a>
@@ -119,7 +119,7 @@ let nba14 = `
 for(const ele of document.getElementsByClassName("nba-14")){ele.innerHTML=(nba14)};
 
 let nba15 = `
-<a href="go:mx15">
+<a href="go:nba15">
 <li class="list-group-item">
 <i class="us flag"></i> NBA (15) | HD</li>
 </a>
@@ -127,7 +127,7 @@ let nba15 = `
 for(const ele of document.getElementsByClassName("nba-15")){ele.innerHTML=(nba15)};
 
 let nba16 = `
-<a href="go:mx16">
+<a href="go:nba16">
 <li class="list-group-item">
 <i class="us flag"></i> NBA (16) | HD</li>
 </a>
@@ -135,7 +135,7 @@ let nba16 = `
 for(const ele of document.getElementsByClassName("nba-16")){ele.innerHTML=(nba16)};
 
 let nba17 = `
-<a href="go:mx17">
+<a href="go:nba17">
 <li class="list-group-item">
 <i class="us flag"></i> NBA (17) | HD</li>
 </a>
@@ -143,7 +143,7 @@ let nba17 = `
 for(const ele of document.getElementsByClassName("nba-17")){ele.innerHTML=(nba17)};
 
 let nba18 = `
-<a href="go:mx18">
+<a href="go:nba18">
 <li class="list-group-item">
 <i class="us flag"></i> NBA (18) | HD</li>
 </a>
@@ -151,7 +151,7 @@ let nba18 = `
 for(const ele of document.getElementsByClassName("nba-18")){ele.innerHTML=(nba18)};
 
 let nba19 = `
-<a href="go:mx19">
+<a href="go:nba19">
 <li class="list-group-item">
 <i class="us flag"></i> NBA (19) | HD</li>
 </a>
@@ -159,7 +159,7 @@ let nba19 = `
 for(const ele of document.getElementsByClassName("nba-19")){ele.innerHTML=(nba19)};
 
 let nba20 = `
-<a href="go:mx20">
+<a href="go:nba20">
 <li class="list-group-item">
 <i class="us flag"></i> NBA (20) | HD</li>
 </a>
