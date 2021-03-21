710ae4379ba365daa4056d0e414e9e0780f2d70c
Eduardo Perdomo <33327508+eduveel19@users.noreply.github.com>
2021-03-21 13:50:55 -0600

    Update channelbase.js

--- basket/nba/channelbase.js
+++ basket/nba/channelbase.js
@@ -1,7 +1,7 @@
 // VARIABLE
 let base = "//";
 let vrb = "p.";
-let cks = "crackstream.gq/show/";
+let cks = "crackstreams.is/show/";
 let fnl = "/playlist.m3u8";
 //let xurl = base + vrb + cks + show + fnl;
 //console.log(xurl);
