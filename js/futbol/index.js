f87a66ae843833fa5a04d39baa6083856342d0b3
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-06-22 06:33:58 -0600

    asd

---
+++ js/futbol/index.js
@@ -0,0 +1,179 @@
+let futbolBundesliga = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:bundesliga">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img competition bundesliga" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>Bundesliga</h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("futbol-bundesliga")){ele.innerHTML=(futbolBundesliga)};
+
+let futbolChampions = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:ucl">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img competition ucl" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>Champions</h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("futbol-ucl")){ele.innerHTML=(futbolChampions)};
+
+let futbolEuropa = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:uel">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img competition uel" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>Europa</h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("futbol-uel")){ele.innerHTML=(futbolEuropa)};
+
+let futbolFACup = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:facup">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img competition facup" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>FACup</h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("futbol-facup")){ele.innerHTML=(futbolFACup)};
+
+let futbolLaLiga = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:laliga">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img competition laliga" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>LaLiga</h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("futbol-laliga")){ele.innerHTML=(futbolLaLiga)};
+
+let futbolLaLiga2 = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:laliga2">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img competition laliga2" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>LaLiga 2</h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("futbol-laliga2")){ele.innerHTML=(futbolLaLiga2)};
+
+let futbolLigaMX = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:ligamx">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img competition ligamx" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>Liga MX</h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("futbol-ligamx")){ele.innerHTML=(futbolLigaMX)};
+
+let futbolLigue1 = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:ligue1">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img competition ligue1" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>Ligue 1</h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("futbol-ligue1")){ele.innerHTML=(futbolLigue1)};
+
+let futbolPremier = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:premier">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img competition premier" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>Premier</h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("futbol-premier")){ele.innerHTML=(futbolPremier)};
+
+let futbolSAF = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:saf">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img competition saf" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>SAF</h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("futbol-saf")){ele.innerHTML=(futbolSAF)};
+
+let futbolSalvavida = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:salvavida">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img competition salvavida" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>Salvavida</h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("futbol-salvavida")){ele.innerHTML=(futbolSalvavida)};
+
+let futbolSerieA = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:seriea">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img competition seriea" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>Serie A</h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("futbol-seriea")){ele.innerHTML=(futbolSerieA)};
