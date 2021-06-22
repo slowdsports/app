bdc1f990a9c9f9c74852bbeb169cdde62b7b1f71
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-06-22 02:10:39 -0600

    jkhk

---
+++ js/login.js
@@ -0,0 +1,57 @@
+let tudnmx = `
+<a href="go:tudnmx">
+<li class="list-group-item">
+<i class="mx flag"></i> TUDN | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("bar")){ele.innerHTML=(bar)};
+
+
+function validarLogin() {
+  // Set the date we're counting down to
+  // Year, Month ( 0 for January ), Day, Hour, Minute, Second, , Milliseconds
+  //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  //::::::::::::                                       ::::::::::::
+  //::::::::::::              12:00 AM                  ::::::::::::
+  //::::::::::::                                       ::::::::::::
+  //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  //                                   (AAAA, MM, DD, HH, mm, S ));
+  var countDownDate = new Date(Date.UTC(2021, 05, 28, 07, 00, 00));
+
+  // Update the count down every 1 second
+  var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now an the count down date
+    // GMT/UTC Adjustment at the end of the function. 0 = GMT/UTC+0; 1 = GMT/UTC+1.
+    var distance = countDownDate - now - (3600000 * 1);
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    for (const ele of document.getElementsByClassName("validarLogin")) {
+      ele.innerHTML = (days + "<span>d</span> " + hours + "<span>h</span> " +
+        minutes + "<span>m</span> " + seconds + "<span>s</span><br />")
+    }
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+      for (const ele of document.getElementsByClassName("validarLogin")) {
+        ele.innerHTML = "<p class='live-text'>Live <i class='fa fa-circle faa-flash animated'></i></p> ";
+      }
+      if (distance + 7200000 < 0) {
+        for (const allEllements of document.getElementsByClassName("validarLogin")) {
+          allEllements.innerHTML = "Ended";
+        }
+      }
+    }
+  }, 1000);
+}
+
+validarLogin()
