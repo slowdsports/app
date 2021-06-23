b93d7777b33a57d71cffcf4ed64967fe7798bb85
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-06-23 16:43:32 -0600

    as

--- js/tv/otros.js
+++ js/tv/otros.js
@@ -1,5 +1,4 @@
 //ARABIA
-for(const ele of document.getElementsByClassName("tv-telemax")){ele.innerHTML=(Telemax)};
 
 let Bein_ar1 = `
 <div class="card darkolorbg text-white card-canal">
@@ -15,3 +14,18 @@ let Bein_ar1 = `
     </div>
 `
 for(const ele of document.getElementsByClassName("tv-beinar1")){ele.innerHTML=(Bein_ar1)};
+
+let Bein_ar2 = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:beinar2">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img beinn" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>BeIN 2 <i class="flag sa"></i></h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("tv-beinar2")){ele.innerHTML=(Bein_ar2)};
