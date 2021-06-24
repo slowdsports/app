5c5dee10f5dfff7a7578b8f968aa49d70454eeb7
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-06-23 19:28:02 -0600

    as

--- js/tv/es.js
+++ js/tv/es.js
@@ -47,6 +47,22 @@ let Bein = `
 for(const ele of document.getElementsByClassName("tv-beinne")){ele.innerHTML=(Bein)};
 
 
+let BeMad = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:bemad">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img bemad" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>BeMad!</h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("tv-bemad")){ele.innerHTML=(BeMad)};
+
+
 let Cuatro = `
 <div class="card darkolorbg text-white card-canal">
        <a class="card-link text-white" href="go:cuatro">           
