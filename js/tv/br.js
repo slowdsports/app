2ca110e46e4605cd5e79fee18c5dbd3748a62a10
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-06-24 04:30:16 -0600

    sad

--- js/tv/br.js
+++ js/tv/br.js
@@ -14,3 +14,18 @@ let TNTSports = `
     </div>
 `
 for(const ele of document.getElementsByClassName("tv-tntsportsbr")){ele.innerHTML=(TNTSports)};
+
+let UCLShow = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:uclshow">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img ucl" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>UCL Show <i class="flag br"></i></h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("tv-uclshow")){ele.innerHTML=(UCLShow)};
