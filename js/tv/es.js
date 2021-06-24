71e6378ddf84d4ba4aa5091e8b8a8cf813fef4fb
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-06-23 20:17:56 -0600

    as

--- js/tv/es.js
+++ js/tv/es.js
@@ -111,6 +111,22 @@ let Sexta = `
 for(const ele of document.getElementsByClassName("tv-sexta")){ele.innerHTML=(Sexta)};
 
 
+let Toros = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:toros">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img toros" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>Toros</h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("tv-toros")){ele.innerHTML=(Toros)};
+
+
 let Telecinco = `
 <div class="card darkolorbg text-white card-canal">
        <a class="card-link text-white" href="go:telecinco">           
