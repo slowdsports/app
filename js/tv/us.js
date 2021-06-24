801764bcca06568d154093846e0ae1d6f0308eff
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-06-23 19:39:17 -0600

    asd

--- js/tv/us.js
+++ js/tv/us.js
@@ -44,6 +44,21 @@ let ESPNU = `
 `
 for(const ele of document.getElementsByClassName("tv-espnu")){ele.innerHTML=(ESPNU)};
 
+let ESPNNews = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:espnnews">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img espn" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>ESPN News <i class="flag us"></i></h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("tv-espnnews")){ele.innerHTML=(ESPNNews)};
+
 let FSUS = `
 <div class="card darkolorbg text-white card-canal">
        <a class="card-link text-white" href="go:fsus">           
@@ -73,3 +88,34 @@ let FS2US = `
     </div>
 `
 for(const ele of document.getElementsByClassName("tv-fs2us")){ele.innerHTML=(FS2US)};
+
+let FSNews = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:fsnews">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img foxsports" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>FSNews <i class="flag us"></i></h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("tv-fsnews")){ele.innerHTML=(FSNews)};
+
+
+let NBCSN = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:nbcsn">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img nbc" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>NBCSN <i class="flag us"></i></h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("tv-nbcsn")){ele.innerHTML=(NBCSN)};
