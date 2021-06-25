aa3ad864e530238504b93314c82f784a256ee246
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-06-25 14:12:41 -0600

    sf

---
+++ js/jjoo.js
@@ -0,0 +1,15 @@
+let BasketJJOO = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:acb">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img jjoo" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>Tokyo</h5></div>
+        </div>
+       </a>
+    </div>
+`;
+
+for(const ele of document.getElementsByClassName("basket-jjoo")){ele.innerHTML=(BasketJJOO)};
