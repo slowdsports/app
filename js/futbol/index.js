88d21d3e1e0b2a2e7ad396f4eda0563a13a710c8
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-06-25 06:02:18 -0600

    as

--- js/futbol/index.js
+++ js/futbol/index.js
@@ -13,6 +13,21 @@ let futbolAmistosos = `
 `
 for(const ele of document.getElementsByClassName("futbol-amistosos")){ele.innerHTML=(futbolAmistosos)};
 
+let futbolCopaOro = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:copaoro">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img competition copaoro" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>Copa Oro</h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("futbol-copaoro")){ele.innerHTML=(futbolCopaOro)};
+
 let futbolBundesliga = `
 <div class="card darkolorbg text-white card-canal">
        <a class="card-link text-white" href="go:bundesliga">           
