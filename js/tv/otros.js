a355bfce6a8535a93dde3efdb22802250dce329c
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-06-25 23:29:54 -0600

    sad

--- js/tv/otros.js
+++ js/tv/otros.js
@@ -44,3 +44,18 @@ let SwissSports = `
     </div>
 `
 for(const ele of document.getElementsByClassName("tv-swiss")){ele.innerHTML=(SwissSports)};
+
+let MTVEUR = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:mtveur">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img mtv" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>MTV </h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("tv-mtveur")){ele.innerHTML=(MTVEUR)};
