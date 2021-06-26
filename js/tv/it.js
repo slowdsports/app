2bfcb99deda4d3d45eafc897b8a1bd976506c33b
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-06-25 22:25:09 -0600

    sda

--- js/tv/it.js
+++ js/tv/it.js
@@ -28,3 +28,18 @@ let RaiSport2 = `
     </div>
 `
 for(const ele of document.getElementsByClassName("tv-raisport2")){ele.innerHTML=(RaiSport2)};
+
+let MotorTrend = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:motortrend">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img motortrend" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>Motor Trend <i class="flag it"></i></h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("tv-motortrend")){ele.innerHTML=(MotorTrend)};
