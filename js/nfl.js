e244df829bd0389f0f6a25f7776f89c8b49db594
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-06-27 16:32:32 -0600

    sda

---
+++ js/nfl.js
@@ -0,0 +1,289 @@
+let steelers = `
+<figure class="figure">
+<img class="figure img-fluid competition steelers" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Steelers
+</figcaption>
+</figure>
+`;
+let chiefs = `
+<figure class="figure">
+<img class="figure img-fluid competition chiefs" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Chiefs
+</figcaption>
+</figure>
+`;
+let saints = `
+<figure class="figure">
+<img class="figure img-fluid competition saints" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Saints
+</figcaption>
+</figure>
+`;
+let bills = `
+<figure class="figure">
+<img class="figure img-fluid competition bills" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Bills
+</figcaption>
+</figure>
+`;
+let browns = `
+<figure class="figure">
+<img class="figure img-fluid competition browns" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Browns
+</figcaption>
+</figure>
+`;
+let packers = `
+<figure class="figure">
+<img class="figure img-fluid competition packers" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Packers
+</figcaption>
+</figure>
+`;
+let colts = `
+<figure class="figure">
+<img class="figure img-fluid competition colts" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Colts
+</figcaption>
+</figure>
+`;
+let rams = `
+<figure class="figure">
+<img class="figure img-fluid competition rams" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Rams
+</figcaption>
+</figure>
+`;
+let seahawks = `
+<figure class="figure">
+<img class="figure img-fluid competition seahawks" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Seahawks
+</figcaption>
+</figure>
+`;
+let titans = `
+<figure class="figure">
+<img class="figure img-fluid competition titans" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Titans
+</figcaption>
+</figure>
+`;
+let buccaneers = `
+<figure class="figure">
+<img class="figure img-fluid competition buccaneers" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Buccaneers
+</figcaption>
+</figure>
+`;
+let cardinals = `
+<figure class="figure">
+<img class="figure img-fluid competition cardinals" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Cardinals
+</figcaption>
+</figure>
+`;
+let ravens = `
+<figure class="figure">
+<img class="figure img-fluid competition ravens" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Ravens
+</figcaption>
+</figure>
+`;
+let raiders = `
+<figure class="figure">
+<img class="figure img-fluid competition raiders" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Raiders
+</figcaption>
+</figure>
+`;
+let dolphins = `
+<figure class="figure">
+<img class="figure img-fluid competition dolphins" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Dolphins
+</figcaption>
+</figure>
+`;
+let bears = `
+<figure class="figure">
+<img class="figure img-fluid competition bears" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Bears
+</figcaption>
+</figure>
+`;
+let broncos = `
+<figure class="figure">
+<img class="figure img-fluid competition broncos" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Broncos
+</figcaption>
+</figure>
+`;
+let lions = `
+<figure class="figure">
+<img class="figure img-fluid competition lions" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Lions
+</figcaption>
+</figure>
+`;
+let vikings = `
+<figure class="figure">
+<img class="figure img-fluid competition vikings" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Vikings
+</figcaption>
+</figure>
+`;
+let patriots = `
+<figure class="figure">
+<img class="figure img-fluid competition patriots" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Patriots
+</figcaption>
+</figure>
+`;
+let sf49ers = `
+<figure class="figure">
+<img class="figure img-fluid competition sf49ers" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+49ers
+</figcaption>
+</figure>
+`;
+let panthers = `
+<figure class="figure">
+<img class="figure img-fluid competition panthers" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Panthers
+</figcaption>
+</figure>
+`;
+let eagles = `
+<figure class="figure">
+<img class="figure img-fluid competition eagles" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Eagles
+</figcaption>
+</figure>
+`;
+let falcons = `
+<figure class="figure">
+<img class="figure img-fluid competition falcons" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Falcons
+</figcaption>
+</figure>
+`;
+let cowboys = `
+<figure class="figure">
+<img class="figure img-fluid competition cowboys" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Cowboys
+</figcaption>
+</figure>
+`;
+let texans = `
+<figure class="figure">
+<img class="figure img-fluid competition texans" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Texans
+</figcaption>
+</figure>
+`;
+let chargers = `
+<figure class="figure">
+<img class="figure img-fluid competition chargers" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Chargers
+</figcaption>
+</figure>
+`;
+let giants = `
+<figure class="figure">
+<img class="figure img-fluid competition giants" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Giants
+</figcaption>
+</figure>
+`;
+let washington = `
+<figure class="figure">
+<img class="figure img-fluid competition washington" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Washington
+</figcaption>
+</figure>
+`;
+let bengals = `
+<figure class="figure">
+<img class="figure img-fluid competition bengals" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Bengals
+</figcaption>
+</figure>
+`;
+let jaguars = `
+<figure class="figure">
+<img class="figure img-fluid competition jaguars" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Jaguars
+</figcaption>
+</figure>
+`;
+let jets = `
+<figure class="figure">
+<img class="figure img-fluid competition jets" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Jets
+</figcaption>
+</figure>
+`;
+
+for (const ele of document.getElementsByClassName("nfl-steelers")){ele.innerHTML=(steelers)}
+for (const ele of document.getElementsByClassName("nfl-chiefs")){ele.innerHTML=(chiefs)}
+for (const ele of document.getElementsByClassName("nfl-saints")){ele.innerHTML=(saints)}
+for (const ele of document.getElementsByClassName("nfl-bills")){ele.innerHTML=(bills)}
+for (const ele of document.getElementsByClassName("nfl-browns")){ele.innerHTML=(browns)}
+for (const ele of document.getElementsByClassName("nfl-packers")){ele.innerHTML=(packers)}
+for (const ele of document.getElementsByClassName("nfl-colts")){ele.innerHTML=(colts)}
+for (const ele of document.getElementsByClassName("nfl-rams")){ele.innerHTML=(rams)}
+for (const ele of document.getElementsByClassName("nfl-seahawks")){ele.innerHTML=(seahawks)}
+for (const ele of document.getElementsByClassName("nfl-titans")){ele.innerHTML=(titans)}
+for (const ele of document.getElementsByClassName("nfl-buccaneers")){ele.innerHTML=(buccaneers)}
+for (const ele of document.getElementsByClassName("nfl-cardinals")){ele.innerHTML=(cardinals)}
+for (const ele of document.getElementsByClassName("nfl-ravens")){ele.innerHTML=(ravens)}
+for (const ele of document.getElementsByClassName("nfl-raiders")){ele.innerHTML=(raiders)}
+for (const ele of document.getElementsByClassName("nfl-dolphins")){ele.innerHTML=(dolphins)}
+for (const ele of document.getElementsByClassName("nfl-bears")){ele.innerHTML=(bears)}
+for (const ele of document.getElementsByClassName("nfl-broncos")){ele.innerHTML=(broncos)}
+for (const ele of document.getElementsByClassName("nfl-lions")){ele.innerHTML=(lions)}
+for (const ele of document.getElementsByClassName("nfl-vikings")){ele.innerHTML=(vikings)}
+for (const ele of document.getElementsByClassName("nfl-patriots")){ele.innerHTML=(patriots)}
+for (const ele of document.getElementsByClassName("nfl-sf49ers")){ele.innerHTML=(sf49ers)}
+for (const ele of document.getElementsByClassName("nfl-panthers")){ele.innerHTML=(panthers)}
+for (const ele of document.getElementsByClassName("nfl-eagles")){ele.innerHTML=(eagles)}
+for (const ele of document.getElementsByClassName("nfl-falcons")){ele.innerHTML=(falcons)}
+for (const ele of document.getElementsByClassName("nfl-cowboys")){ele.innerHTML=(cowboys)}
+for (const ele of document.getElementsByClassName("nfl-texans")){ele.innerHTML=(texans)}
+for (const ele of document.getElementsByClassName("nfl-chargers")){ele.innerHTML=(chargers)}
+for (const ele of document.getElementsByClassName("nfl-giants")){ele.innerHTML=(giants)}
+for (const ele of document.getElementsByClassName("nfl-washington")){ele.innerHTML=(washington)}
+for (const ele of document.getElementsByClassName("nfl-bengals")){ele.innerHTML=(bengals)}
+for (const ele of document.getElementsByClassName("nfl-jaguars")){ele.innerHTML=(jaguars)}
+for (const ele of document.getElementsByClassName("nfl-jets")){ele.innerHTML=(jets)}
