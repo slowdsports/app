05bfc8d7763292af15a7fbad7d1e6483411fc03b
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-06-27 21:58:10 -0600

    Update sudamericana.js

--- js/futbol/sudamericana.js
+++ js/futbol/sudamericana.js
@@ -1,9 +1,340 @@
-let sociedad = `
+let rosario = `
 <figure class="figure">
-<img class="figure img-fluid competition sociedad" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<img class="figure img-fluid competition suda-rosario" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
 <figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
-Sociedad
+rosario
 </figcaption>
 </figure>
 `;
-for (const ele of document.getElementsByClassName("sud-holstein")){ele.innerHTML=(sociedad)};
+for (const ele of document.getElementsByClassName("suda-rosario")){ele.innerHTML=(rosario)};
+
+let huachipato = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-huachipato" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+huachipato
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-huachipato")){ele.innerHTML=(huachipato)};
+
+let sanlorenzo = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-sanlorenzo" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+sanlorenzo
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-sanlorenzo")){ele.innerHTML=(sanlorenzo)};
+
+let doceoctubre = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-doceoctubre" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+12 octubre
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-doceoctubre")){ele.innerHTML=(doceoctubre)};
+
+
+
+let independiente = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-independiente" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+independiente
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-independiente")){ele.innerHTML=(independiente)};
+
+let torque = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-torque" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+torque
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-torque")){ele.innerHTML=(torque)};
+
+let bahia = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-bahia" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+bahía
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-bahia")){ele.innerHTML=(bahia)};
+
+let guabira = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-guabira" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+guabira
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-guabira")){ele.innerHTML=(guabira)};
+
+
+
+
+let arsenal = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-arsenal" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+arsenal
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-arsenal")){ele.innerHTML=(arsenal)};
+
+let ceara = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-ceara" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+ceara
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-ceara")){ele.innerHTML=(ceara)};
+
+let bolivar = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-bolivar" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+bolivar
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-bolivar")){ele.innerHTML=(bolivar)};
+
+let wilstermann = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-wilstermann" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+wilstermann
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-wilstermann")){ele.innerHTML=(wilstermann)};
+
+
+
+
+let athletico = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-athletico" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+athletico
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-athletico")){ele.innerHTML=(athletico)};
+
+let melgar = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-melgar" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+melgar
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-melgar")){ele.innerHTML=(melgar)};
+
+let aucas = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-aucas" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+aucas
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-aucas")){ele.innerHTML=(aucas)};
+
+let metropolitanos = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-metropolitanos" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+metropolitanos
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-metropolitanos")){ele.innerHTML=(metropolitanos)};
+
+
+
+
+let penarol = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-penarol" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+penarol
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-penarol")){ele.innerHTML=(penarol)};
+
+let corinthians = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-corinthians" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+corinthians
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-corinthians")){ele.innerHTML=(corinthians)};
+
+let riverpe = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-riverpe" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+riverpe
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-riverpe")){ele.innerHTML=(riverpe)};
+
+let huancayo = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-huancayo" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+huancayo
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-huancayo")){ele.innerHTML=(huancayo)};
+
+
+
+
+let libertad = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-libertad" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+libertad
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-libertad")){ele.innerHTML=(libertad)};
+
+let goianiense = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-goianiense" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+goianiense
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-goianiense")){ele.innerHTML=(goianiense)};
+
+let newells = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-newells" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+newells
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-newells")){ele.innerHTML=(newells)};
+
+let palestino = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-palestino" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+palestino
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-palestino")){ele.innerHTML=(palestino)};
+
+
+
+
+let bragantino = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-bragantino" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+bragantino
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-bragantino")){ele.innerHTML=(bragantino)};
+
+let emelec = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-emelec" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+emelec
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-emelec")){ele.innerHTML=(emelec)};
+
+let talleres = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-talleres" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+talleres
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-talleres")){ele.innerHTML=(talleres)};
+
+let tolima = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-tolima" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+tolima
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-tolima")){ele.innerHTML=(tolima)};
+
+
+
+
+let gremio = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-gremio" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+gremio
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-gremio")){ele.innerHTML=(gremio)};
+
+let lanus = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-lanus" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+lanus
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-lanus")){ele.innerHTML=(lanus)};
+
+let equidad = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-equidad" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+equidad
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-equidad")){ele.innerHTML=(equidad)};
+
+let aragua = `
+<figure class="figure">
+<img class="figure img-fluid competition suda-aragua" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+aragua
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("suda-aragua")){ele.innerHTML=(aragua)};
+
