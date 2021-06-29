4c292d6582bfc2e11f46e07ba7d9567afff8fecf
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-06-29 04:16:02 -0600

    sd

--- js/resumen/elementos.js
+++ js/resumen/elementos.js
@@ -13,6 +13,36 @@ let elementoFutbol = `
 `
 for(const ele of document.getElementsByClassName("resumen-futbol")){ele.innerHTML=(elementoFutbol)};
 
+let futbolrEurocopa = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="eurocopa/main.html">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img competition eurocopa" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>Eurocopa</h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("futbolr-eurocopa")){ele.innerHTML=(futbolrEurocopa)};
+
+let futbolrCopaAmerica = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="camerica/main.html">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img competition copaamerica" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>Copa América</h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("futbolr-copaamerica")){ele.innerHTML=(futbolrCopaAmerica)};
+
 let elementoBasket = `
 <div class="card darkolorbg text-white card-canal">
        <a class="card-link text-white" href="go:basketr">           
