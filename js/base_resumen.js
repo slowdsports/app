ff295c50b788af027e848a495423188def6e937c
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-06-29 02:32:11 -0600

    sda

--- js/base_resumen.js
+++ js/base_resumen.js
@@ -1,5 +1,5 @@
 // VARIABLE
 let base = "//app.slidepresenter.com/api/gateway/files/video/";
-let fnl = "/converted.MP4";
+let ext = "/converted.MP4";
 //let xurl = base + vrb + cks + show + fnl;
 //console.log(xurl);
