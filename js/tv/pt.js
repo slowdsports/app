892ddd244d31545f9dde911e18555589ed139bb7
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-07-01 09:29:36 -0600

    sad

--- js/tv/pt.js
+++ js/tv/pt.js
@@ -1,15 +1,45 @@
 // PORTUGAL
 let SPORTTV1 = `
 <div class="card darkolorbg text-white card-canal">
-       <a class="card-link text-white" href="go:nbcsn">           
+       <a class="card-link text-white" href="go:sporttv">           
         <div class="card-body">
             <div class="d-flex justify-content-center">
                 <img class="canal-img sporttv1" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
             </div>
             
-            <div class="d-flex justify-content-center"><h5>Sporttv1 </h5></div>
+            <div class="d-flex justify-content-center"><h5>Sporttv1 <i class="flag pt"></i></h5></div>
         </div>
        </a>
     </div>
 `
 for(const ele of document.getElementsByClassName("tv-sporttv1")){ele.innerHTML=(SPORTTV1)};
+
+let SPORTTV2 = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:sporttv2">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img sporttv" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>Sporttv2 <i class="flag pt"></i></h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("tv-sporttv2")){ele.innerHTML=(SPORTTV2)};
+
+let SPORTTV3 = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:sporttv3">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img sporttv" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>Sporttv3 <i class="flag pt"></i></h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("tv-sporttv3")){ele.innerHTML=(SPORTTV3)};
