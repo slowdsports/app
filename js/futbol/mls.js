acc041236cd7db656ea696665d704f7bf45a6f7a
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-07-01 11:17:48 -0600

    asd

--- js/futbol/mls.js
+++ js/futbol/mls.js
@@ -1,9 +1,270 @@
-let rosario = `
+let columbus = `
 <figure class="figure">
-<img class="figure img-fluid competition rosario" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<img class="figure img-fluid competition columbus" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
 <figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
-rosario
+columbus
 </figcaption>
 </figure>
 `;
-for (const ele of document.getElementsByClassName("sudamerica-rosario")){ele.innerHTML=(rosario)};
+for (const ele of document.getElementsByClassName("major-columbus")){ele.innerHTML=(columbus)};
+
+let kansas = `
+<figure class="figure">
+<img class="figure img-fluid competition kansas" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+kansas
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-kansas")){ele.innerHTML=(kansas)};
+
+let minnesota = `
+<figure class="figure">
+<img class="figure img-fluid competition minnesota" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+minnesota
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-minnesota")){ele.innerHTML=(minnesota)};
+
+let portland = `
+<figure class="figure">
+<img class="figure img-fluid competition portland" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+portland
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-portland")){ele.innerHTML=(portland)};
+
+let lafc = `
+<figure class="figure">
+<img class="figure img-fluid competition lafc" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+LAFC
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-lafc")){ele.innerHTML=(columbus)};
+
+let toronto = `
+<figure class="figure">
+<img class="figure img-fluid competition toronto" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+toronto
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-toronto")){ele.innerHTML=(toronto)};
+
+let seattle = `
+<figure class="figure">
+<img class="figure img-fluid competition seattle" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+seattle
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-seattle")){ele.innerHTML=(seattle)};
+
+let orlando = `
+<figure class="figure">
+<img class="figure img-fluid competition orlando" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+orlando
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-orlando")){ele.innerHTML=(orlando)};
+
+let sanjose = `
+<figure class="figure">
+<img class="figure img-fluid competition sanjose" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+San José
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-sanjose")){ele.innerHTML=(sanjose)};
+
+let philadelphia = `
+<figure class="figure">
+<img class="figure img-fluid competition philadelphia" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+philadelphia
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-philadelphia")){ele.innerHTML=(philadelphia)};
+
+let montreal = `
+<figure class="figure">
+<img class="figure img-fluid competition montreal" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+montreal
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-montreal")){ele.innerHTML=(montreal)};
+
+let colorado = `
+<figure class="figure">
+<img class="figure img-fluid competition colorado" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+colorado
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-colorado")){ele.innerHTML=(colorado)};
+
+let nyrb = `
+<figure class="figure">
+<img class="figure img-fluid competition nyrb" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+NYRB
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-nyrb")){ele.innerHTML=(nyrb)};
+
+let atlanta = `
+<figure class="figure">
+<img class="figure img-fluid competition atlanta" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+atlanta
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-atlanta")){ele.innerHTML=(atlanta)};
+
+let vancouver = `
+<figure class="figure">
+<img class="figure img-fluid competition vancouver" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+vancouver
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-vancouver")){ele.innerHTML=(vancouver)};
+
+let cincinnati = `
+<figure class="figure">
+<img class="figure img-fluid competition cincinnati" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+cincinnati
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-cincinnati")){ele.innerHTML=(cincinnati)};
+
+let saltlake = `
+<figure class="figure">
+<img class="figure img-fluid competition saltlake" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+salt lake
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-saltlake")){ele.innerHTML=(saltlake)};
+
+let newengland = `
+<figure class="figure">
+<img class="figure img-fluid competition newengland" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+new england
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-newengland")){ele.innerHTML=(newengland)};
+
+let dallas = `
+<figure class="figure">
+<img class="figure img-fluid competition dallas" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+dallas
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-dallas")){ele.innerHTML=(dallas)};
+
+let dcunited = `
+<figure class="figure">
+<img class="figure img-fluid competition dcunited" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+DCU
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-dcunited")){ele.innerHTML=(dcunited)};
+
+let nashville = `
+<figure class="figure">
+<img class="figure img-fluid competition nashville" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+nashville
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-nashville")){ele.innerHTML=(nashville)};
+
+let chicago = `
+<figure class="figure">
+<img class="figure img-fluid competition chicago" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+chicago
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-chicago")){ele.innerHTML=(chicago)};
+
+let nycfc = `
+<figure class="figure">
+<img class="figure img-fluid competition nycfc" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+NYCFC
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-nycfc")){ele.innerHTML=(nycfc)};
+
+let houston = `
+<figure class="figure">
+<img class="figure img-fluid competition houston" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+houston
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-houston")){ele.innerHTML=(houston)};
+
+let lagalaxy = `
+<figure class="figure">
+<img class="figure img-fluid competition lagalaxy" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+galaxy
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-lagalaxy")){ele.innerHTML=(lagalaxy)};
+
+let intermiami = `
+<figure class="figure">
+<img class="figure img-fluid competition intermiami" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+inter
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-intermiami")){ele.innerHTML=(intermiami)};
+
+let austin = `
+<figure class="figure">
+<img class="figure img-fluid competition austin" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+austin
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("major-austin")){ele.innerHTML=(austin)};
+
