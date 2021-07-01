23d759fb30a5e46c235c79cd27907b02fef0c3b0
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-07-01 09:34:58 -0600

    Update uk.js

---
+++ js/tv/uk.js
@@ -0,0 +1,31 @@
+// REINO UNIDO
+
+let BTSport1 = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:btsport1">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img btsport" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>BT Sport </h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("tv-btsport1")){ele.innerHTML=(BTSport1)};
+
+let BTSport2 = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:btsport2">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img btsport" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>BT Sport 2 </h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("tv-btsport2")){ele.innerHTML=(BTSport2)};
