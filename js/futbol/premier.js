602fc898add00e59fbf0e5ad2b21e477f5e4292f
Eduardo <eperdomo833@gmail.com>
2021-07-02 21:07:30 -0600

    Update premier.js

--- js/futbol/premier.js
+++ js/futbol/premier.js
@@ -142,7 +142,7 @@ Norwich
 </figcaption>
 </figure>
 `;
-let brenford = `
+let brentford = `
 <figure class="figure">
 <img class="figure img-fluid competition brentford" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
 <figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
