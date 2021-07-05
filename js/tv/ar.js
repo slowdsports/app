4d3b297172a14b2d3cce70d962e07ada24ec3f0c
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-07-04 18:40:24 -0600

    Update ar.js

--- js/tv/ar.js
+++ js/tv/ar.js
@@ -14,6 +14,21 @@ let ESPNSUR = `
 `
 for(const ele of document.getElementsByClassName("tv-espnsur")){ele.innerHTML=(ESPNSUR)};
 
+let ESPNSURIF = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:espnsurif">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img espn" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>ESPN <i class="flag ar"></i></h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("tv-espnsurif")){ele.innerHTML=(ESPNSURIF)};
+
 let ESPN2SUR = `
 <div class="card darkolorbg text-white card-canal">
        <a class="card-link text-white" href="go:espn2sur">           
@@ -29,6 +44,21 @@ let ESPN2SUR = `
 `
 for(const ele of document.getElementsByClassName("tv-espn2sur")){ele.innerHTML=(ESPN2SUR)};
 
+let ESPN2SURIF = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:espn2surif">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img espn" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>ESPN 2 <i class="flag ar"></i></h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("tv-espn2surif")){ele.innerHTML=(ESPN2SURIF)};
+
 let ESPN3SUR = `
 <div class="card darkolorbg text-white card-canal">
        <a class="card-link text-white" href="go:espn3sur">           
@@ -43,6 +73,21 @@ let ESPN3SUR = `
     </div>
 `
 for(const ele of document.getElementsByClassName("tv-espn3sur")){ele.innerHTML=(ESPN3SUR)};
+
+let ESPN3SURIF = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:espn3surif">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img espn" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>ESPN 3 <i class="flag ar"></i></h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("tv-espn3surif")){ele.innerHTML=(ESPN3SURIF)};
 
 let ESPNExtra = `
 <div class="card darkolorbg text-white card-canal">
