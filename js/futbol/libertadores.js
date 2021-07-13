fbb0b6e6ec8040f902c85880616b8e37a49a4b4f
Eduardo <eperdomo833@gmail.com>
2021-07-12 22:51:23 -0600

    Update libertadores.js

--- js/futbol/libertadores.js
+++ js/futbol/libertadores.js
@@ -2,8 +2,79 @@ let argentinos = `
 <figure class="figure">
 <img class="figure img-fluid competition argentinos" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
 <figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
-argentinos
+Argentinos
 </figcaption>
 </figure>
 `;
 for (const ele of document.getElementsByClassName("libertador-argentinos")){ele.innerHTML=(argentinos)};
+
+let boca = `
+<figure class="figure">
+<img class="figure img-fluid competition boca" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Boca 
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("libertador-boca")){ele.innerHTML=(boca)};
+
+let mineiro = `
+<figure class="figure">
+<img class="figure img-fluid competition mineiro" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Mineiro
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("libertador-mineiro")){ele.innerHTML=(mineiro)};
+
+let cerro = `
+<figure class="figure">
+<img class="figure img-fluid competition cerro" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Cerro
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("libertador-cerro")){ele.innerHTML=(cerro)};
+
+let saopaulo = `
+<figure class="figure">
+<img class="figure img-fluid competition saopaulo" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Sao Paulo
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("libertador-saopaulo")){ele.innerHTML=(saopaulo)};
+
+let river = `
+<figure class="figure">
+<img class="figure img-fluid competition river" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+River
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("libertador-river")){ele.innerHTML=(river)};
+
+
+let velez = `
+<figure class="figure">
+<img class="figure img-fluid competition velez" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Velez
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("libertador-velez")){ele.innerHTML=(velez)};
+
+let barcelona = `
+<figure class="figure">
+<img class="figure img-fluid competition barcelona" width="48" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+<figcaption style="margin-bottom: -10%; margin-top: 8%; margin-right: 5px;" class="figure-caption">
+Barcelona
+</figcaption>
+</figure>
+`;
+for (const ele of document.getElementsByClassName("libertador-barcelona")){ele.innerHTML=(barcelona)};
