43618af118c41007e26ace22ca7cfb236eb80330
Alex <slowdsports@gmail.com>
2021-07-13 09:55:51 -0600

    Update mx.js

--- js/tv/mx.js
+++ js/tv/mx.js
@@ -1,4 +1,19 @@
 // MÉXICO
+let ComedyCentral = `
+<div class="card darkolorbg text-white card-canal">
+       <a class="card-link text-white" href="go:comedyc">           
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <img class="canal-img comedy" src="https://i.ibb.co/w0qg9JF/trans.png" alt="">
+            </div>
+            
+            <div class="d-flex justify-content-center"><h5>ComedyCentral <i class="flag mx"></i></h5></div>
+        </div>
+       </a>
+    </div>
+`
+for(const ele of document.getElementsByClassName("tv-comedyc")){ele.innerHTML=(ComedyCentral)};
+
 let FOX = `
 <div class="card darkolorbg text-white card-canal">
        <a class="card-link text-white" href="go:fox">           
