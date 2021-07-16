19395b9afecd10e3e623fde2c133c2cf84677c6d
Alex <slowdsports@gmail.com>
2021-07-15 21:39:37 -0600

    gdfs

--- tv/p/chn.js
+++ tv/p/chn.js
@@ -1,8 +1,8 @@
+
 // F O X
 
-let fsoul = "//lnc-fox-soul-scte.tubi.video/index.m3u8";
+var fsoul = "https://lnc-fox-soul-scte.tubi.video/index.m3u8";
 let fseattle = "//lnc-kcpq-fox-aws.tubi.video/index.m3u8";
-let fdallas = "//lnc-kdfw-fox-aws.tubi.video/index.m3u8";
 let fdallas = "//lnc-kdfw-fox-aws.tubi.video/index.m3u8";
 let fminneapolis = "//lnc-kmsp-fox-aws.tubi.video/index.m3u8";
 let fhouston = "//lnc-kriv-fox-aws.tubi.video/index.m3u8";
@@ -22,3 +22,37 @@ let fmiami = "//bcsecurelivehls-i.akamaihd.net/hls/live/598043/4368278029001_1/o
 let fwashington = "//lnc-wttg-fox-aws.tubi.video/index.m3u8";
 let ftampa = "//lnc-wtvt-fox-aws.tubi.video/index.m3u8";
 let fphiladelphia = "//lnc-wtxf-fox-aws.tubi.video/index.m3u8";
+
+
+
+function getParameterByName(name) {
+              name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+              var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+                  results = regex.exec(location.search);
+              return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+          }
+
+var getURL = getParameterByName('get');
+        if (getURL == "#") {alert('El canal se encuentra Offline');}
+        if (getURL == "") {alert('El canal se encuentra Offline');}
+        if (getURL == "fsoul") {getURL = fsoul;}
+        if (getURL == "fseattle") {getURL = fseattle;}
+        if (getURL == "fdallas") {getURL = fdallas;}
+        if (getURL == "fminneapolis") {getURL = fminneapolis;}
+        if (getURL == "fhouston") {getURL = fhouston;}
+        if (getURL == "fphoenix") {getURL = fphoenix;}
+        if (getURL == "faustin") {getURL = faustin;}
+        if (getURL == "flosangeles") {getURL = flosangeles;}
+        if (getURL == "fsanfrancisco") {getURL = fsanfrancisco;}
+        if (getURL == "fatlanta") {getURL = fatlanta;}
+        if (getURL == "fchicago") {getURL = fchicago;}
+        if (getURL == "fmilwaukee") {getURL = fmilwaukee;}
+        if (getURL == "fdetroit") {getURL = fdetroit;}
+        if (getURL == "fnewyork") {getURL = fnewyork;}
+        if (getURL == "forlando") {getURL = forlando;}
+        if (getURL == "fsacannah") {getURL = fsacannah;}
+        if (getURL == "ftulsa") {getURL = ftulsa;}
+        if (getURL == "fmiami") {getURL = fmiami;}
+        if (getURL == "fwashington") {getURL = fwashington;}
+        if (getURL == "ftampa") {getURL = ftampa;}
+        if (getURL == "fphiladelphia") {getURL = fphiladelphia;}
