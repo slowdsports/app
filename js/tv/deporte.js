ce79651437726d7555d1ec983f00883f4910aa3f
Eduardo <eperdomo833@gmail.com>
2021-07-19 16:18:47 -0600

    Update deporte.js

--- js/tv/deporte.js
+++ js/tv/deporte.js
@@ -11,7 +11,403 @@ let canalesTVDeporte = `
 
    <!--CANAL-->
   <div class="col-6 col-sm-4">  
-    <span class="tv-comedyc"></span>
+    <span class="tv-vamos"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-abc"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-barcatvb"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-beinne"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-bein1ar"></span>
+  </div>
+   <!--/CANAL-->
+    
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-bein2ar"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-beinxtra"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-beinxtraes"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-btsport1"></span>
+  </div>
+   <!--/CANAL-->
+    
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-btsport2"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-cbsh"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-daznf1esb"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-daznf1s"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-espnbr"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-espnsurs"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-espnsurif"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-espnus"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-espn2sur"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-espn2surif"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-espn2sur"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-espn3sur"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-espn3surif"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-espnxtra"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-espnu"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-espnnews"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-eurosport1"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-eurosport1es"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-eurosport2"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-eurosport2es"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-esport3"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-foxsports"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-fsus"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-fs2us"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-fsnews"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-fsplus"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-mlstv"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-mmatv"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-megaes"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-mdepb"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-mlaliga"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-msnbc"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-nbcus"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-ncbscn"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-nbclx"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-onegolf"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-raisport2"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-redbulltv"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-sevillatv"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-swiss"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-sky1racing"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-sky2racing"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-thouroughb"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-ssports"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-sportitali"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-sporttv1"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-sporttv2"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-sporttv3"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-tdp"></span>
+  </div>
+   <!--/CANAL-->
+    
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-tdpgeo"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-tsn1if"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-tsn2if"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-tsn3if"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-tntsportsb"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-tntsportsc"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-tudn"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-win"></span>
+  </div>
+   <!--/CANAL-->
+
+    <!--CANAL-->
+  <div class="col-6 col-sm-4">  
+    <span class="tv-winp"></span>
   </div>
    <!--/CANAL-->
   
