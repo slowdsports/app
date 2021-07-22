6badfe7eefb749d63ab61b910f2289ba5259a192
Eduardo <eperdomo833@gmail.com>
2021-07-21 22:43:38 -0600

    Update canales.js

--- js/tv/canales.js
+++ js/tv/canales.js
@@ -1058,12 +1058,17 @@ let telecadena = `
 <i class="hn flag"></i> Telecadena | HD</li>
 </a>
 `;
-let telemundon = `
-<a href="go:telemundon">
+let telemundop = `
+<a href="go:telemundop">
 <li class="canalito list-group-item darkolorbg blancolor">
 <i class="mx flag"></i> Telemundo | HD</li>
 </a>
 `;
+let nbcu = `
+<a href="go:nbcu">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="mx flag"></i> Universo | HD</li>
+</a>
 let tigohn = `
 <a href="go:tigohn">
 <li class="canalito list-group-item darkolorbg blancolor">
@@ -1323,6 +1328,7 @@ for(const ele of document.getElementsByClassName("mdepb")){ele.innerHTML=(mdepb)
 for(const ele of document.getElementsByClassName("mdep1")){ele.innerHTML=(mdep1)};
 for(const ele of document.getElementsByClassName("mdep2")){ele.innerHTML=(mdep2)};
 for(const ele of document.getElementsByClassName("mdep3")){ele.innerHTML=(mdep3)};
+for(const ele of document.getElementsByClassName("nbcu")){ele.innerHTML=(nbcu)};
 for(const ele of document.getElementsByClassName("mlcamp")){ele.innerHTML=(mlcamp)};
 for(const ele of document.getElementsByClassName("mlcamp1")){ele.innerHTML=(mlcamp1)};
 for(const ele of document.getElementsByClassName("mlcamp2")){ele.innerHTML=(mlcamp2)};
@@ -1334,7 +1340,7 @@ for(const ele of document.getElementsByClassName("dazn2es")){ele.innerHTML=(dazn
 for(const ele of document.getElementsByClassName("movistar")){ele.innerHTML=(movistar)};
 for(const ele of document.getElementsByClassName("tdtvp")){ele.innerHTML=(tdtvp)};
 for(const ele of document.getElementsByClassName("telecadena")){ele.innerHTML=(telecadena)};
-for(const ele of document.getElementsByClassName("telemundon")){ele.innerHTML=(telemundon)};
+for(const ele of document.getElementsByClassName("telemundop")){ele.innerHTML=(telemundop)};
 for(const ele of document.getElementsByClassName("tigohn")){ele.innerHTML=(tigohn)};
 for(const ele of document.getElementsByClassName("tigopy")){ele.innerHTML=(tigopy)};
 for(const ele of document.getElementsByClassName("tigobo")){ele.innerHTML=(tigobo)};
